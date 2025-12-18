d002b3f428dbe23d9982eb66b576823ce04509b9
commit <lubloodz@gmail.com>
2025-12-17 22:55:19 -0800

    Added LiveDataTestUtil for testing LiveData

---
+++ app/src/androidTest/java/com/example/cst338_f25b_group2_project02/LiveDataTestUtil.java
@@ -0,0 +1,35 @@
+package com.example.cst338_f25b_group2_project02;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.Observer;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Utility for testing LiveData
+ */
+public class LiveDataTestUtil {
+
+    /**
+     * Gets value from LiveData synchronously for testing
+     */
+    public static <T> T getValue(final LiveData<T> liveData) throws InterruptedException {
+        final Object[] data = new Object[1];
+        final CountDownLatch latch = new CountDownLatch(1);
+
+        Observer<T> observer = new Observer<T>() {
+            @Override
+            public void onChanged(T o) {
+                data[0] = o;
+                latch.countDown();
+                liveData.removeObserver(this);
+            }
+        };
+
+        liveData.observeForever(observer);
+        latch.await(2, TimeUnit.SECONDS);
+
+        return (T) data[0];
+    }
+}
